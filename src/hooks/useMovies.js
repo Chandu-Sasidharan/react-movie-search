38483ea6f60f992468212e05fc2970765f00d0f2
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-10-13 20:59:29 +0200

    add User Agent header for trakt api call

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -6,7 +6,9 @@ export default function useMovies(searchTerm) {
     const fetchMovies = async () => {
         // If there is no searchTerm, fetch trending movies
         const traktUrl = searchTerm
-            ? `https://api.trakt.tv/search/movie?query=${encodeURIComponent(searchTerm)}`
+            ? `https://api.trakt.tv/search/movie?query=${encodeURIComponent(
+                  searchTerm
+              )}`
             : 'https://api.trakt.tv/movies/trending';
 
         // Fetch movie data from Trakt API
@@ -15,16 +17,17 @@ export default function useMovies(searchTerm) {
                 'Content-Type': 'application/json',
                 'trakt-api-version': '2',
                 'trakt-api-key': traktClientId,
+                'User-Agent': 'react-movie-search/1.0.0',
             },
         });
 
         // Extract IMDb IDs from the fetched data
         const movieIds = data
             .map(({ movie }) => movie.ids.imdb)
-            .filter(id => id !== null && id !== undefined);
+            .filter((id) => id !== null && id !== undefined);
 
         // Create an array of promises to fetch details from OMDB API
-        const movieDetailPromises = movieIds.map(id =>
+        const movieDetailPromises = movieIds.map((id) =>
             axios.get(`https://www.omdbapi.com/?i=${id}&apikey=${omdbApiKey}`)
         );
 
@@ -33,9 +36,13 @@ export default function useMovies(searchTerm) {
 
         // Filter out invalid responses and extract valid movie data
         const movieDetails = responses
-            .filter(response => 
-                response.status === 200 && response.data && response.data.Response !== "False")
-            .map(response => response.data);
+            .filter(
+                (response) =>
+                    response.status === 200 &&
+                    response.data &&
+                    response.data.Response !== 'False'
+            )
+            .map((response) => response.data);
 
         return movieDetails;
     };
